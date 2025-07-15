cbaed011f2a5fc2ffb602ff4653909908b359147
hitanshu <ahk0976@gmail.com>
2025-07-15 13:12:24 +0530

    first commit

---
+++ peeso/peeso/doctype/loan_agreement/loan_agreement.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, hitanshu and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class LoanAgreement(Document):
+	pass
