60759a3bbe277246c05608ecca805935ebb15f09
hitanshu <ahk0976@gmail.com>
2025-08-18 16:25:06 +0530

    Deleted Doctype

--- peeso/peeso/doctype/google_location/google_location.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, hitanshu and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Google Location", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- peeso/peeso/doctype/google_location/test_google_location.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, hitanshu and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestGoogleLocation(FrappeTestCase):
-	pass
