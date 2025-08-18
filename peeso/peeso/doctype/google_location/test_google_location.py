e10a23dbc960c9835a6c92358da1bae26c688cce
hitanshu <ahk0976@gmail.com>
2025-08-18 16:19:32 +0530

    Added google_location Doctype and updated customer, field_verification, vehicle_master

---
+++ peeso/peeso/doctype/google_location/test_google_location.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, hitanshu and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGoogleLocation(FrappeTestCase):
+	pass
